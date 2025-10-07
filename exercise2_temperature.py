e6464cd41ca900eec3265ba355b7c8a0212def37
Chinwendu <queenezikeanyi@gmail.com>
2025-10-07 18:35:32 +0100

    add conversion functions

---
+++ exercise2_temperature.py
@@ -0,0 +1,18 @@
+def convertToFahrenheit(degreesCelsius):
+    
+    # Calculate and return the degrees Fahrenheit:
+
+    return degreesCelsius * (9 / 5) + 32
+
+ 
+
+ 
+
+def convertToCelsius(degreesFahrenheit):
+
+    # Calculate and return the degrees Celsius:
+
+    return (degreesFahrenheit - 32) * (5 / 9)
+
+
+print(convertToCelsius(100))
